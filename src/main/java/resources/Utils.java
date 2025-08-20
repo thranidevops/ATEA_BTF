fd9784c42a1cc2d9d3b6c86afb6f9620125ef6b3
Thara <tharani_vig@yahoo.co.in>
2025-08-19 22:52:27 -0700

    rearranging structure

--- src/main/java/resources/Utils.java
+++ src/main/java/resources/Utils.java
@@ -40,7 +40,7 @@ public class Utils {
 	public static String getGlobalValue(String key) throws IOException
 	{
 		Properties prop =new Properties();
-		FileInputStream fis =new FileInputStream("C:/Users/Tharani/eclipse-workspace-ATEA-BTF/MyAPIProjectBTF/src/test/java/resources/global.properties");
+		FileInputStream fis =new FileInputStream("C:/Users/Tharani/eclipse-workspace-ATEA-BTF/MyAPIProjectBTF/src/test/resources/global.properties");
 		prop.load(fis);
 		return prop.getProperty(key);
 	
